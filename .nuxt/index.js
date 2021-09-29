575dc960c64b35d38a0438807ae69ae8ea8e4691
Quang Henry <quangvm2000@gmail.com>
2021-09-29 10:13:57 +0700

    Initialize project

---
+++ .nuxt/index.js
@@ -0,0 +1,304 @@
+import Vue from 'vue'
+
+import Meta from 'vue-meta'
+import ClientOnly from 'vue-client-only'
+import NoSsr from 'vue-no-ssr'
+import { createRouter } from './router.js'
+import NuxtChild from './components/nuxt-child.js'
+import NuxtError from '..\\layouts\\error.vue'
+import Nuxt from './components/nuxt.js'
+import App from './App.js'
+import { setContext, getLocation, getRouteData, normalizeError } from './utils'
+
+/* Plugins */
+
+import nuxt_plugin_vuescrollto_540aedc0 from 'nuxt_plugin_vuescrollto_540aedc0' // Source: .\\vue-scrollto.js (mode: 'client')
+import nuxt_plugin_cookieuniversalnuxt_09bea671 from 'nuxt_plugin_cookieuniversalnuxt_09bea671' // Source: .\\cookie-universal-nuxt.js (mode: 'all')
+import nuxt_plugin_pluginutils_43737e76 from 'nuxt_plugin_pluginutils_43737e76' // Source: .\\nuxt-i18n\\plugin.utils.js (mode: 'all')
+import nuxt_plugin_pluginseo_03b9a159 from 'nuxt_plugin_pluginseo_03b9a159' // Source: .\\nuxt-i18n\\plugin.seo.js (mode: 'all')
+import nuxt_plugin_pluginrouting_1bb9b0d0 from 'nuxt_plugin_pluginrouting_1bb9b0d0' // Source: .\\nuxt-i18n\\plugin.routing.js (mode: 'all')
+import nuxt_plugin_pluginmain_2f096c9f from 'nuxt_plugin_pluginmain_2f096c9f' // Source: .\\nuxt-i18n\\plugin.main.js (mode: 'all')
+import nuxt_plugin_nuxtplugin50e62083_70e7be5a from 'nuxt_plugin_nuxtplugin50e62083_70e7be5a' // Source: .\\nuxt.plugin.50e62083.js (mode: 'all')
+import nuxt_plugin_plugin_744235b7 from 'nuxt_plugin_plugin_744235b7' // Source: .\\composition-api\\plugin.js (mode: 'all')
+import nuxt_plugin_pluginsi18ncookiesf96dcce6_756fa02d from 'nuxt_plugin_pluginsi18ncookiesf96dcce6_756fa02d' // Source: .\\plugins.i18n-cookies.f96dcce6.js (mode: 'all')
+import nuxt_plugin_pluginse2etestingc40295f4_125da65e from 'nuxt_plugin_pluginse2etestingc40295f4_125da65e' // Source: .\\plugins.e2e-testing.c40295f4.js (mode: 'all')
+import nuxt_plugin_pluginsloggerd44a8f46_f5a12a3c from 'nuxt_plugin_pluginsloggerd44a8f46_f5a12a3c' // Source: .\\plugins.logger.d44a8f46.js (mode: 'all')
+import nuxt_plugin_pluginsssrbdc4ff22_d766711c from 'nuxt_plugin_pluginsssrbdc4ff22_d766711c' // Source: .\\plugins.ssr.bdc4ff22.js (mode: 'all')
+import nuxt_plugin_pluginscontext163412b2_63c89f4b from 'nuxt_plugin_pluginscontext163412b2_63c89f4b' // Source: .\\plugins.context.163412b2.js (mode: 'all')
+import nuxt_plugin_workbox_55c9c0b6 from 'nuxt_plugin_workbox_55c9c0b6' // Source: .\\workbox.js (mode: 'client')
+import nuxt_plugin_metaplugin_1edac1b6 from 'nuxt_plugin_metaplugin_1edac1b6' // Source: .\\pwa\\meta.plugin.js (mode: 'all')
+import nuxt_plugin_iconplugin_158b76ce from 'nuxt_plugin_iconplugin_158b76ce' // Source: .\\pwa\\icon.plugin.js (mode: 'all')
+import nuxt_plugin_scrollToTopclient_6f28b416 from 'nuxt_plugin_scrollToTopclient_6f28b416' // Source: ..\\plugins\\scrollToTop.client.js (mode: 'client')
+
+// Component: <ClientOnly>
+Vue.component(ClientOnly.name, ClientOnly)
+
+// TODO: Remove in Nuxt 3: <NoSsr>
+Vue.component(NoSsr.name, {
+  ...NoSsr,
+  render (h, ctx) {
+    if (process.client && !NoSsr._warned) {
+      NoSsr._warned = true
+
+      console.warn('<no-ssr> has been deprecated and will be removed in Nuxt 3, please use <client-only> instead')
+    }
+    return NoSsr.render(h, ctx)
+  }
+})
+
+// Component: <NuxtChild>
+Vue.component(NuxtChild.name, NuxtChild)
+Vue.component('NChild', NuxtChild)
+
+// Component NuxtLink is imported in server.js or client.js
+
+// Component: <Nuxt>
+Vue.component(Nuxt.name, Nuxt)
+
+Object.defineProperty(Vue.prototype, '$nuxt', {
+  get() {
+    const globalNuxt = this.$root.$options.$nuxt
+    if (process.client && !globalNuxt && typeof window !== 'undefined') {
+      return window.$nuxt
+    }
+    return globalNuxt
+  },
+  configurable: true
+})
+
+Vue.use(Meta, {"keyName":"head","attribute":"data-n-head","ssrAttribute":"data-n-head-ssr","tagIDKeyName":"hid"})
+
+const defaultTransition = {"name":"page","mode":"out-in","appear":false,"appearClass":"appear","appearActiveClass":"appear-active","appearToClass":"appear-to"}
+
+async function createApp(ssrContext, config = {}) {
+  const router = await createRouter(ssrContext, config)
+
+  // Create Root instance
+
+  // here we inject the router and store to all child components,
+  // making them available everywhere as `this.$router` and `this.$store`.
+  const app = {
+    head: {"title":"Shopify | Vue Storefront Next","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"name":"theme-color","content":"#5ece7b"},{"hid":"description","name":"description","content":"Vue Storefront 2 template for Shopify."},{"name":"generator","content":"Vue Storefront 2"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"},{"rel":"preconnect","href":"https:\u002F\u002Ffonts.gstatic.com","crossorigin":"crossorigin"},{"rel":"preload","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap","as":"style"},{"rel":"stylesheet","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap","media":"print","onload":"this.media='all'"}],"style":[],"script":[]},
+
+    router,
+    nuxt: {
+      defaultTransition,
+      transitions: [defaultTransition],
+      setTransitions (transitions) {
+        if (!Array.isArray(transitions)) {
+          transitions = [transitions]
+        }
+        transitions = transitions.map((transition) => {
+          if (!transition) {
+            transition = defaultTransition
+          } else if (typeof transition === 'string') {
+            transition = Object.assign({}, defaultTransition, { name: transition })
+          } else {
+            transition = Object.assign({}, defaultTransition, transition)
+          }
+          return transition
+        })
+        this.$options.nuxt.transitions = transitions
+        return transitions
+      },
+
+      err: null,
+      dateErr: null,
+      error (err) {
+        err = err || null
+        app.context._errored = Boolean(err)
+        err = err ? normalizeError(err) : null
+        let nuxt = app.nuxt // to work with @vue/composition-api, see https://github.com/nuxt/nuxt.js/issues/6517#issuecomment-573280207
+        if (this) {
+          nuxt = this.nuxt || this.$options.nuxt
+        }
+        nuxt.dateErr = Date.now()
+        nuxt.err = err
+        // Used in src/server.js
+        if (ssrContext) {
+          ssrContext.nuxt.error = err
+        }
+        return err
+      }
+    },
+    ...App
+  }
+
+  const next = ssrContext ? ssrContext.next : location => app.router.push(location)
+  // Resolve route
+  let route
+  if (ssrContext) {
+    route = router.resolve(ssrContext.url).route
+  } else {
+    const path = getLocation(router.options.base, router.options.mode)
+    route = router.resolve(path).route
+  }
+
+  // Set context to app.context
+  await setContext(app, {
+    route,
+    next,
+    error: app.nuxt.error.bind(app),
+    payload: ssrContext ? ssrContext.payload : undefined,
+    req: ssrContext ? ssrContext.req : undefined,
+    res: ssrContext ? ssrContext.res : undefined,
+    beforeRenderFns: ssrContext ? ssrContext.beforeRenderFns : undefined,
+    ssrContext
+  })
+
+  function inject(key, value) {
+    if (!key) {
+      throw new Error('inject(key, value) has no key provided')
+    }
+    if (value === undefined) {
+      throw new Error(`inject('${key}', value) has no value provided`)
+    }
+
+    key = '$' + key
+    // Add into app
+    app[key] = value
+    // Add into context
+    if (!app.context[key]) {
+      app.context[key] = value
+    }
+
+    // Check if plugin not already installed
+    const installKey = '__nuxt_' + key + '_installed__'
+    if (Vue[installKey]) {
+      return
+    }
+    Vue[installKey] = true
+    // Call Vue.use() to install the plugin into vm
+    Vue.use(() => {
+      if (!Object.prototype.hasOwnProperty.call(Vue.prototype, key)) {
+        Object.defineProperty(Vue.prototype, key, {
+          get () {
+            return this.$root.$options[key]
+          }
+        })
+      }
+    })
+  }
+
+  // Inject runtime config as $config
+  inject('config', config)
+
+  // Add enablePreview(previewData = {}) in context for plugins
+  if (process.static && process.client) {
+    app.context.enablePreview = function (previewData = {}) {
+      app.previewData = Object.assign({}, previewData)
+      inject('preview', previewData)
+    }
+  }
+  // Plugin execution
+
+  if (process.client && typeof nuxt_plugin_vuescrollto_540aedc0 === 'function') {
+    await nuxt_plugin_vuescrollto_540aedc0(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_cookieuniversalnuxt_09bea671 === 'function') {
+    await nuxt_plugin_cookieuniversalnuxt_09bea671(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginutils_43737e76 === 'function') {
+    await nuxt_plugin_pluginutils_43737e76(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginseo_03b9a159 === 'function') {
+    await nuxt_plugin_pluginseo_03b9a159(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginrouting_1bb9b0d0 === 'function') {
+    await nuxt_plugin_pluginrouting_1bb9b0d0(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginmain_2f096c9f === 'function') {
+    await nuxt_plugin_pluginmain_2f096c9f(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_nuxtplugin50e62083_70e7be5a === 'function') {
+    await nuxt_plugin_nuxtplugin50e62083_70e7be5a(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_plugin_744235b7 === 'function') {
+    await nuxt_plugin_plugin_744235b7(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginsi18ncookiesf96dcce6_756fa02d === 'function') {
+    await nuxt_plugin_pluginsi18ncookiesf96dcce6_756fa02d(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginse2etestingc40295f4_125da65e === 'function') {
+    await nuxt_plugin_pluginse2etestingc40295f4_125da65e(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginsloggerd44a8f46_f5a12a3c === 'function') {
+    await nuxt_plugin_pluginsloggerd44a8f46_f5a12a3c(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginsssrbdc4ff22_d766711c === 'function') {
+    await nuxt_plugin_pluginsssrbdc4ff22_d766711c(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_pluginscontext163412b2_63c89f4b === 'function') {
+    await nuxt_plugin_pluginscontext163412b2_63c89f4b(app.context, inject)
+  }
+
+  if (process.client && typeof nuxt_plugin_workbox_55c9c0b6 === 'function') {
+    await nuxt_plugin_workbox_55c9c0b6(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_metaplugin_1edac1b6 === 'function') {
+    await nuxt_plugin_metaplugin_1edac1b6(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_iconplugin_158b76ce === 'function') {
+    await nuxt_plugin_iconplugin_158b76ce(app.context, inject)
+  }
+
+  if (process.client && typeof nuxt_plugin_scrollToTopclient_6f28b416 === 'function') {
+    await nuxt_plugin_scrollToTopclient_6f28b416(app.context, inject)
+  }
+
+  // Lock enablePreview in context
+  if (process.static && process.client) {
+    app.context.enablePreview = function () {
+      console.warn('You cannot call enablePreview() outside a plugin.')
+    }
+  }
+
+  // Wait for async component to be resolved first
+  await new Promise((resolve, reject) => {
+    // Ignore 404s rather than blindly replacing URL in browser
+    if (process.client) {
+      const { route } = router.resolve(app.context.route.fullPath)
+      if (!route.matched.length) {
+        return resolve()
+      }
+    }
+    router.replace(app.context.route.fullPath, resolve, (err) => {
+      // https://github.com/vuejs/vue-router/blob/v3.4.3/src/util/errors.js
+      if (!err._isRouter) return reject(err)
+      if (err.type !== 2 /* NavigationFailureType.redirected */) return resolve()
+
+      // navigated to a different route in router guard
+      const unregister = router.afterEach(async (to, from) => {
+        if (process.server && ssrContext && ssrContext.url) {
+          ssrContext.url = to.fullPath
+        }
+        app.context.route = await getRouteData(to)
+        app.context.params = to.params || {}
+        app.context.query = to.query || {}
+        unregister()
+        resolve()
+      })
+    })
+  })
+
+  return {
+    app,
+    router
+  }
+}
+
+export { createApp, NuxtError }
---
+++ .nuxt/router.js
@@ -0,0 +1,305 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _645e80cd = () => interopDefault(import('..\\pages\\Category.vue' /* webpackChunkName: "pages/Category" */))
+const _b3f8c95a = () => interopDefault(import('..\\pages\\Checkout.vue' /* webpackChunkName: "" */))
+const _79323352 = () => interopDefault(import('..\\pages\\Checkout\\Billing.vue' /* webpackChunkName: "" */))
+const _17e1855d = () => interopDefault(import('..\\pages\\Checkout\\Payment.vue' /* webpackChunkName: "" */))
+const _26632532 = () => interopDefault(import('..\\pages\\Checkout\\Shipping.vue' /* webpackChunkName: "" */))
+const _53478de4 = () => interopDefault(import('..\\pages\\Checkout\\ThankYou.vue' /* webpackChunkName: "" */))
+const _bbb4a5d6 = () => interopDefault(import('..\\pages\\Checkout.vue' /* webpackChunkName: "pages/Checkout" */))
+const _9c65e3d4 = () => interopDefault(import('..\\pages\\Checkout\\Billing.vue' /* webpackChunkName: "pages/Checkout/Billing" */))
+const _507c6021 = () => interopDefault(import('..\\pages\\Checkout\\Payment.vue' /* webpackChunkName: "pages/Checkout/Payment" */))
+const _70e229ba = () => interopDefault(import('..\\pages\\Checkout\\Shipping.vue' /* webpackChunkName: "pages/Checkout/Shipping" */))
+const _9dc6926c = () => interopDefault(import('..\\pages\\Checkout\\ThankYou.vue' /* webpackChunkName: "pages/Checkout/ThankYou" */))
+const _3dfb011f = () => interopDefault(import('..\\pages\\ContactUs.vue' /* webpackChunkName: "pages/ContactUs" */))
+const _287550ec = () => interopDefault(import('..\\pages\\Home.vue' /* webpackChunkName: "" */))
+const _1b4ad8a4 = () => interopDefault(import('..\\pages\\Home.vue' /* webpackChunkName: "pages/Home" */))
+const _44ab88bc = () => interopDefault(import('..\\pages\\MyAccount.vue' /* webpackChunkName: "pages/MyAccount" */))
+const _66c87f41 = () => interopDefault(import('..\\pages\\MyAccount\\AdressBook.vue' /* webpackChunkName: "pages/MyAccount/AdressBook" */))
+const _f488e9ee = () => interopDefault(import('..\\pages\\MyAccount\\BillingDetails.vue' /* webpackChunkName: "pages/MyAccount/BillingDetails" */))
+const _7646dba4 = () => interopDefault(import('..\\pages\\MyAccount\\LoyaltyCard.vue' /* webpackChunkName: "pages/MyAccount/LoyaltyCard" */))
+const _a31b2272 = () => interopDefault(import('..\\pages\\MyAccount\\MyNewsletter.vue' /* webpackChunkName: "pages/MyAccount/MyNewsletter" */))
+const _c84810ea = () => interopDefault(import('..\\pages\\MyAccount\\MyProfile.vue' /* webpackChunkName: "pages/MyAccount/MyProfile" */))
+const _39424e06 = () => interopDefault(import('..\\pages\\MyAccount\\MyReviews.vue' /* webpackChunkName: "pages/MyAccount/MyReviews" */))
+const _ec90dfb0 = () => interopDefault(import('..\\pages\\MyAccount\\OrderHistory.vue' /* webpackChunkName: "pages/MyAccount/OrderHistory" */))
+const _3cdd843c = () => interopDefault(import('..\\pages\\MyAccount\\ShippingDetails.vue' /* webpackChunkName: "pages/MyAccount/ShippingDetails" */))
+const _6b0b6620 = () => interopDefault(import('..\\pages\\Product.vue' /* webpackChunkName: "pages/Product" */))
+// const _e372a566 = () => interopDefault(import('..\\pages\\ResetPassword.vue' /* webpackChunkName: "" */))
+const _558a1524 = () => interopDefault(import('..\\pages\\MyAccount.vue' /* webpackChunkName: "" */))
+const _07b2c21c = () => interopDefault(import('..\\pages\\Product.vue' /* webpackChunkName: "" */))
+const _683c6f0b = () => interopDefault(import('..\\pages\\Category.vue' /* webpackChunkName: "" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/Category",
+    component: _645e80cd,
+    name: "Category___en"
+  }, {
+    path: "/checkout",
+    component: _b3f8c95a,
+    name: "checkout___en",
+    children: [{
+      path: "billing",
+      component: _79323352,
+      name: "billing___en"
+    }, {
+      path: "payment",
+      component: _17e1855d,
+      name: "payment___en"
+    }, {
+      path: "shipping",
+      component: _26632532,
+      name: "shipping___en"
+    }, {
+      path: "thank-you",
+      component: _53478de4,
+      name: "thank-you___en"
+    }]
+  }, {
+    path: "/Checkout",
+    component: _bbb4a5d6,
+    name: "Checkout___en",
+    children: [{
+      path: "Billing",
+      component: _9c65e3d4,
+      name: "Checkout-Billing___en"
+    }, {
+      path: "Payment",
+      component: _507c6021,
+      name: "Checkout-Payment___en"
+    }, {
+      path: "Shipping",
+      component: _70e229ba,
+      name: "Checkout-Shipping___en"
+    }, {
+      path: "ThankYou",
+      component: _9dc6926c,
+      name: "Checkout-ThankYou___en"
+    }]
+  }, {
+    path: "/ContactUs",
+    component: _3dfb011f,
+    name: "ContactUs___en"
+  }, {
+    path: "/de",
+    component: _287550ec,
+    name: "home___de"
+  }, {
+    path: "/Home",
+    component: _1b4ad8a4,
+    name: "Home___en"
+  }, {
+    path: "/MyAccount",
+    component: _44ab88bc,
+    name: "MyAccount___en",
+    children: [{
+      path: "AdressBook",
+      component: _66c87f41,
+      name: "MyAccount-AdressBook___en"
+    }, {
+      path: "BillingDetails",
+      component: _f488e9ee,
+      name: "MyAccount-BillingDetails___en"
+    }, {
+      path: "LoyaltyCard",
+      component: _7646dba4,
+      name: "MyAccount-LoyaltyCard___en"
+    }, {
+      path: "MyNewsletter",
+      component: _a31b2272,
+      name: "MyAccount-MyNewsletter___en"
+    }, {
+      path: "MyProfile",
+      component: _c84810ea,
+      name: "MyAccount-MyProfile___en"
+    }, {
+      path: "MyReviews",
+      component: _39424e06,
+      name: "MyAccount-MyReviews___en"
+    }, {
+      path: "OrderHistory",
+      component: _ec90dfb0,
+      name: "MyAccount-OrderHistory___en"
+    }, {
+      path: "ShippingDetails",
+      component: _3cdd843c,
+      name: "MyAccount-ShippingDetails___en"
+    }]
+  }, {
+    path: "/Product",
+    component: _6b0b6620,
+    name: "Product___en"
+  },
+  // {
+  //   path: "/reset-password",
+  //   component: _e372a566,
+  //   name: "reset-password___en"
+  // },
+  {
+    path: "/de/Category",
+    component: _645e80cd,
+    name: "Category___de"
+  }, {
+    path: "/de/checkout",
+    component: _b3f8c95a,
+    name: "checkout___de",
+    children: [{
+      path: "billing",
+      component: _79323352,
+      name: "billing___de"
+    }, {
+      path: "payment",
+      component: _17e1855d,
+      name: "payment___de"
+    }, {
+      path: "shipping",
+      component: _26632532,
+      name: "shipping___de"
+    }, {
+      path: "thank-you",
+      component: _53478de4,
+      name: "thank-you___de"
+    }]
+  }, {
+    path: "/de/Checkout",
+    component: _bbb4a5d6,
+    name: "Checkout___de",
+    children: [{
+      path: "Billing",
+      component: _9c65e3d4,
+      name: "Checkout-Billing___de"
+    }, {
+      path: "Payment",
+      component: _507c6021,
+      name: "Checkout-Payment___de"
+    }, {
+      path: "Shipping",
+      component: _70e229ba,
+      name: "Checkout-Shipping___de"
+    }, {
+      path: "ThankYou",
+      component: _9dc6926c,
+      name: "Checkout-ThankYou___de"
+    }]
+  }, {
+    path: "/de/ContactUs",
+    component: _3dfb011f,
+    name: "ContactUs___de"
+  }, {
+    path: "/de/Home",
+    component: _1b4ad8a4,
+    name: "Home___de"
+  }, {
+    path: "/de/MyAccount",
+    component: _44ab88bc,
+    name: "MyAccount___de",
+    children: [{
+      path: "AdressBook",
+      component: _66c87f41,
+      name: "MyAccount-AdressBook___de"
+    }, {
+      path: "BillingDetails",
+      component: _f488e9ee,
+      name: "MyAccount-BillingDetails___de"
+    }, {
+      path: "LoyaltyCard",
+      component: _7646dba4,
+      name: "MyAccount-LoyaltyCard___de"
+    }, {
+      path: "MyNewsletter",
+      component: _a31b2272,
+      name: "MyAccount-MyNewsletter___de"
+    }, {
+      path: "MyProfile",
+      component: _c84810ea,
+      name: "MyAccount-MyProfile___de"
+    }, {
+      path: "MyReviews",
+      component: _39424e06,
+      name: "MyAccount-MyReviews___de"
+    }, {
+      path: "OrderHistory",
+      component: _ec90dfb0,
+      name: "MyAccount-OrderHistory___de"
+    }, {
+      path: "ShippingDetails",
+      component: _3cdd843c,
+      name: "MyAccount-ShippingDetails___de"
+    }]
+  }, {
+    path: "/de/Product",
+    component: _6b0b6620,
+    name: "Product___de"
+  },
+  // {
+  //   path: "/de/reset-password",
+  //   component: _e372a566,
+  //   name: "reset-password___de"
+  // },
+  {
+    path: "/de/my-account/:pageName?",
+    component: _558a1524,
+    name: "my-account___de"
+  }, {
+    path: "/de/p/:id/:slug",
+    component: _07b2c21c,
+    name: "product___de"
+  }, {
+    path: "/de/c/:slug_1/:slug_2?/:slug_3?/:slug_4?/:slug_5?",
+    component: _683c6f0b,
+    name: "category___de"
+  }, {
+    path: "/my-account/:pageName?",
+    component: _558a1524,
+    name: "my-account___en"
+  }, {
+    path: "/p/:id/:slug",
+    component: _07b2c21c,
+    name: "product___en"
+  }, {
+    path: "/c/:slug_1/:slug_2?/:slug_3?/:slug_4?/:slug_5?",
+    component: _683c6f0b,
+    name: "category___en"
+  }, {
+    path: "/",
+    component: _287550ec,
+    name: "home___en"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
